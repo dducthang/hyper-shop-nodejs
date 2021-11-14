df823d661ad4eb9e743e7d58b9e9ad6ce57266ff
Deone <dangthang013@gmail.com>
2021-11-14 14:58:29 +0700

    remove database connection

--- app.js
+++ app.js
@@ -2,14 +2,7 @@ require('dotenv/config');
 
 const path = require('path');
 
-const mongoose = require('./db/mongoose.js');
 const express = require('express');
-const Product = require('./models/product.js');
-const User = require('./models/user.js');
-const Comment = require('./models/comment.js');
-const Cart = require('./models/cart.js');
-const Order = require('./models/order.js');
-const OrderItems = require('./models/orderItem.js');
 
 const shopRoutes = require('./routes/shop');
 
@@ -21,35 +14,9 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use(shopRoutes);
 
-//routes
-
-//database
-
-//insert db
-// const doc = new Cart({
-//   user: '6189f44e806c3f4aae64da89',
-//   orderItems: ['618a538d641982874a53e0ec'],
-// });
-
-// doc.save().then(() => {
-//   console.log('saved');
-// });
-
-//test
-// Comment.find({}, (e, c) => {
-//   console.log(c);
-// })
-//   .populate('user')
-//   .exec((e, u) => {
-//     if (e) console.log(e);
-//     else console.log(u);
-//   });
-
-Cart.find({}, (e, c) => {
-  console.log(c);
-});
 //server;
 app.listen(process.env.PORT || 4000, () => {
   console.log('Server is running on http://localhost:3000');
