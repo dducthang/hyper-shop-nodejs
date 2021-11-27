7fdbc9852ae50e4ce57414c8ab5f8bded2faaec6
Deone <dangthang013@gmail.com>
2021-11-27 17:51:29 +0700

    thang - catch exception upload inappropriate image file type

---
+++ public/js/custom/add-product.js
@@ -0,0 +1,16 @@
+const productName = document.getElementById("productName");
+const brand = document.getElementById("brand");
+const price = document.getElementById("price");
+const color = document.getElementById("color");
+const gender = document.getElementById("gender");
+const height = document.getElementById("height");
+const closure = document.getElementById("closure");
+const material = document.getElementById("material");
+const category = document.getElementById("category");
+const addProductForm = document.getElementById("addProductForm");
+
+const addProductHandler=()=>{
+    
+}
+
+addProductForm.addEventListener('submit', addProductHandler)
