ca85247838703c24fea453247cade8f1ddc885d0
Deone <dangthang013@gmail.com>
2021-12-07 00:03:43 +0700

    thang - display admin infor

--- controllers/user.js
+++ controllers/user.js
@@ -2,11 +2,13 @@
 exports.getUserList = (req,res,next)=>{
     res.render('shop/userList', {
         pageTitle:'User list',
+        user: req.user
     });
 }
 
 exports.getUserDetail = (req,res,next)=>{
     res.render('shop/userDetail', {
         pageTitle:'User Profile',
+        user: req.user
     });
 }
