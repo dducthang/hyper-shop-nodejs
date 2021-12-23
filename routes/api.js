d106b7703fdea2af4f2b4283551b97843357f921
Tung Nguyen <ncnt257@gmail.com>
2021-12-23 23:37:40 +0700

    add response

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const productsApiController = require('../controllers/apiController/products');
+const responseApiController = require('../controllers/apiController/response');
 
 router.get('/products', productsApiController.getProductsApi);
+router.post('/response/:commentId', responseApiController.postResponse);
 
 module.exports = router;
