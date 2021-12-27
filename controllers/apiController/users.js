bbcbf27d78beeb71720e445d10c239622794a2ed
TheNam74 <nguyenthenam1910@gmail.com>
2021-12-27 22:36:54 +0700

    pagination user list - view user details

--- controllers/apiController/users.js
+++ controllers/apiController/users.js
@@ -1,10 +1,20 @@
 const UserService = require("../../models/services/userService");
 exports.getUsersApi = async (req, res, next) => {
-  const page = req.query.page;
-  console.log("---------------------page: ", page);
-  const users = await UserService.getUsers();
-  //   console.log(users);
+  const ITEMS_PER_PAGE = 2;
+  let page = req.query.page;
+  let users = await UserService.getUsers();
+  const usersCount = users.length;
+
+  if (page === "First") page = 1;
+  if (page === "Last") page = Math.ceil(usersCount / ITEMS_PER_PAGE);
+
+  users = await UserService.getUsers()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE);
+
   res.status(200).send({
     users,
+    page,
+    lastPage: Math.ceil(usersCount / ITEMS_PER_PAGE),
   });
 };
