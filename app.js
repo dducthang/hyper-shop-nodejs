7cd8fa23eedf9cfc118b199f0fdf4f64f09be339
Tung Nguyen <ncnt257@gmail.com>
2022-01-11 11:49:20 +0700

    add favicon

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const passport = require("passport");
-const session = require("express-session");
-const flash = require("express-flash");
-const MongoDbStore = require("connect-mongodb-session")(session);
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('express-flash');
+const MongoDbStore = require('connect-mongodb-session')(session);
+const favicon = require('serve-favicon');
 
-require("./db/mongoose.js");
-require("dotenv/config");
+require('./db/mongoose.js');
+require('dotenv/config');
 
-const checkAuthenticated = require("./config/auth").checkAuthenticated;
-const initializePassport = require("./config/passport");
+const checkAuthenticated = require('./config/auth').checkAuthenticated;
+const initializePassport = require('./config/passport');
 initializePassport(passport);
 
-const shopRoutes = require("./routes/shop");
-const productRoutes = require("./routes/product");
-const userRoutes = require("./routes/user");
-const authRoutes = require("./routes/auth");
-const orderRoutes = require("./routes/order");
-const apiRoutes = require("./routes/api");
-const adminRoutes = require("./routes/admin");
+const shopRoutes = require('./routes/shop');
+const productRoutes = require('./routes/product');
+const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
+const orderRoutes = require('./routes/order');
+const apiRoutes = require('./routes/api');
+const adminRoutes = require('./routes/admin');
 
 const app = express();
 const port = process.env.PORT || 4000;
 const store = new MongoDbStore({
   uri: process.env.CONNECTION_STRING,
-  collection: "sessions", //tên bảng lưu session trong mongo là sessions
+  collection: 'sessions', //tên bảng lưu session trong mongo là sessions
 });
 //middlewares
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); //để parse request về json
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(flash());
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(
   session({
@@ -49,15 +51,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //routes
-app.use("/", shopRoutes);
-app.use("/products", checkAuthenticated, productRoutes);
-app.use("/users", checkAuthenticated, userRoutes);
-app.use("/auth", authRoutes);
-app.use("/orders", checkAuthenticated, orderRoutes);
-app.use("/api", checkAuthenticated, apiRoutes);
-app.use("/admins", checkAuthenticated, adminRoutes);
+app.use('/', shopRoutes);
+app.use('/products', checkAuthenticated, productRoutes);
+app.use('/users', checkAuthenticated, userRoutes);
+app.use('/auth', authRoutes);
+app.use('/orders', checkAuthenticated, orderRoutes);
+app.use('/api', checkAuthenticated, apiRoutes);
+app.use('/admins', checkAuthenticated, adminRoutes);
 
 //server;
 app.listen(process.env.PORT || 4000, () => {
-  console.log("Server is running on http://localhost:4000");
+  console.log('Server is running on http://localhost:4000');
 });
